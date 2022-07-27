b31b813598bf062954821a653c95f585a7e31cee
narhiril <roxannarusbarsky@yahoo.com>
2022-07-26 23:14:02 -0500

    through part 1, cargo hold

---
+++ src/app/equipment/equipment-item.ts
@@ -0,0 +1,10 @@
+//class for equippables
+
+export class EquipmentItem {
+    name: string;
+    mass: number;
+    constructor(name: string, mass: number) {
+        this.name = name;
+        this.mass = mass;
+    }
+}
--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipmentItem } from './equipment-item';
 
 @Component({
   selector: 'app-equipment',
@@ -6,26 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
+   equipmentItems: EquipmentItem[] = [
+       new EquipmentItem('Duct Tape', 0.5),
+       new EquipmentItem('Space Camera', 20),
+       new EquipmentItem('Food', 150),
+       new EquipmentItem('Oxygen Tanks', 400),
+       new EquipmentItem('AE-35 Unit', 5),
+       new EquipmentItem('ISS Supplies', 800),
+       new EquipmentItem('Water', 250),
+       new EquipmentItem('Satellite', 1200),
+       new EquipmentItem('R2 Unit', 32)
    ];
    cargoHold: object[] = [];
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   isNearMaxMass: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
 
-   // Code your addItem function here:
+   addItem(item: EquipmentItem): boolean {
+        this.cargoMass += item.mass;
+        this.cargoHold.push(item);
+        return (this.maximumAllowedMass - this.cargoMass <= 200);
+   }
+
+   canAdd(item: EquipmentItem): boolean {
+        return (
+             this.cargoMass + item.mass <= this.maximumAllowedMass 
+          && this.cargoHold.length !== this.maxItems
+        );
+   }
+
+   emptyHold(): void {
+        this.cargoHold = [];
+        this.cargoMass = 0;
+        this.isNearMaxMass = false;
+   }
    
 }
